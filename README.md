ba3503b3abf865e3302ae63ae86d0bd35e0f0726
GuillaumeGas <guillaume@gas-ntic.fr>
2020-01-18 15:19:39 +0100

    Added screenshots and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # MealPlanner
+
+Petite application web pour m'aider à plannifier les repas de la semaine, le but étant de proposer des plats qui n'ont pas été déjà choisis la semaine passée.
+Elle est codée en PHP et utilise une base de données mysql.
+
+# Installation
+
+J'ai effectué mes tests en utilisant wamp, téléchargeable sur le site officiel.
+
+Après avoir "cloné" de dépôt et installé un environnement web comme wamp:
+ - Installer les tables via le script `bdd/mealplanner.sql`
+ - Modifier le script `mysql_connect.php` avec le couple login/mdp permettant la connexion à la base de données
+ - Télécharger le moteur de template Smarty, et le copier à la racine tel que le chemin suivant existe : MealPlanner/smarty/Smarty.class.php
+
+# Captures d'écran
+
+![](img/screenshots/weeks.bmp)
+
+![](img/screenshots/meals.bmp)
+
+![](img/screenshots/addWeek.bmp)
---
+++ bdd/mealplanner.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3308
+-- Généré le :  sam. 18 jan. 2020 à 14:05
+-- Version du serveur :  8.0.18
+-- Version de PHP :  7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `mealfinder`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient`
+--
+
+DROP TABLE IF EXISTS `ingredient`;
+CREATE TABLE IF NOT EXISTS `ingredient` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(256) NOT NULL,
+  `Type` tinyint(1) NOT NULL,
+  `Unit` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredientsofmeal`
+--
+
+DROP TABLE IF EXISTS `ingredientsofmeal`;
+CREATE TABLE IF NOT EXISTS `ingredientsofmeal` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `IngredientId` int(11) NOT NULL,
+  `MealId` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `meal`
+--
+
+DROP TABLE IF EXISTS `meal`;
+CREATE TABLE IF NOT EXISTS `meal` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(256) NOT NULL,
+  `Type` int(11) NOT NULL,
+  `QuickToMake` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mealsofweek`
+--
+
+DROP TABLE IF EXISTS `mealsofweek`;
+CREATE TABLE IF NOT EXISTS `mealsofweek` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `WeekId` int(11) NOT NULL,
+  `MealId` int(11) NOT NULL,
+  `Day` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `week`
+--
+
+DROP TABLE IF EXISTS `week`;
+CREATE TABLE IF NOT EXISTS `week` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Date` date NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
