5f53dd53f6e82b3b6d938d156210a5121d23d766
GuillaumeGas <guillaume@gas-ntic.fr>
2024-12-03 17:56:14 +0100

    Set table name from mealfinder to mealplanner

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,7 +1,7 @@
 <?php
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=mealfinder;port=3308', 'guidono', '235');
-        $base = "mealfinder";
+        $bdd = new PDO('mysql:host=localhost;dbname=mealplanner;port=3306', 'root', '');
+        $base = "mealplanner";
         $bdd->exec("SET NAMES UTF8");
     } catch( PDOException $Exception ) {
         // Note The Typecast To An Integer!
